faa6e34d1eb19e232ebf7b9e8c860f1a21ba5b9b
Jongyoun Kim <jazzparty72@gmail.com>
2015-09-24 13:34:21 +0900

    delete unkown line

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 //For command line git tutorial
 //first Local repository modification
 //second GitHub modification
->>>>>>> origin/master
 print("Hello World")
 print("Tell your world")
 print("Tell his world")
